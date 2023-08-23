5b820f061ccdca12c1712cdbc479c9660a99b368
ojodedragon <luchosibona@gmail.com>
2023-08-23 15:10:53 -0300

    16 de agosto

--- pantalla.py
+++ pantalla.py
@@ -1,6 +1,37 @@
 import pygame
 import sys
 
+# initialize game engine
+pygame.init()
+
+window_width = 1400
+window_height = 1400
+
+animation_increment = 10
+clock_tick_rate = 20
+
+# Open a window
+size = (window_width, window_height)
+screen = pygame.display.set_mode(size)
+
+# Set title to the window
+pygame.display.set_caption("Hello World")
+
+dead = False
+
+clock = pygame.time.Clock()
+background_image = pygame.image.load('WhatsApp Image 2023-08-23 at 12.07.33.jpeg').convert()
+
+while not dead:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            dead = True
+
+    screen.blit(background_image, [0, 0])
+
+    pygame.display.flip()
+    clock.tick(clock_tick_rate)
+
 pygame.init()
 # Ventana
 width_main = 1365
@@ -15,8 +46,8 @@ gris_oscuro_color = (50, 50, 50)
 imagen_path = "imagen.png"  # Asegúrate de tener la imagen en el mismo directorio que el script
 imagen1 = pygame.image.load('gardevoir.gif')
 imagen1 = pygame.transform.scale(imagen1, (200, 200))
-imagen2 = pygame.image.load('WhatsApp Image 2023-08-23 at 12.07.33.jpeg')
-imagen2 = pygame.transform.scale(imagen2, (1365, 710))  # Redimensionar la imagen a 200x200 píxeles
+
+
 class Window:
     def __init__(self, title, bg_color):
         self.window = pygame.display.set_mode((width_main, height_main))
@@ -27,8 +58,10 @@ class Window:
     def show(self):
         self.window.fill(self.bg_color)
         pygame.display.set_caption(self.title)
-        self.window.blit(imagen1, (width_main//2 - imagen1.get_width()//2, 100))  # Mostrar la imagen en la parte superior
+        self.window.blit(imagen1,
+                         (width_main // 2 - imagen1.get_width() // 2, 100))  # Mostrar la imagen en la parte superior
         pygame.display.flip()
+
 
 class Button:
     def __init__(self, x, y, width, height, image, callback):
@@ -45,6 +78,7 @@ class Button:
                 x, y = event.pos
                 if self.rect.collidepoint(x, y):
                     self.callback()
+
 
 # Funciones para mostrar las ventanas de colores
 def show_lila_window():
@@ -64,6 +98,7 @@ def show_lila_window():
 
     pygame.quit()
 
+
 def show_celeste_window():
     celeste_window = Window("Ventana Celeste", celeste_color)
     button_grey = Button(25, 25, 50, 50, gris_oscuro_color, show_grey_window)
@@ -79,6 +114,7 @@ def show_celeste_window():
         imagen1.fill((0, 0, 0, 0))
     pygame.quit()
 
+
 def show_grey_window():
     grey_window = Window("Ventana Gris Oscuro", gris_oscuro_color)
     running = True
@@ -92,10 +128,12 @@ def show_grey_window():
     pygame.quit()
 
 
-main_window = Window("*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*", (255, 255, 255))
-button_lila = Button(width_main//2 - 125, height_main//2 - 25, 100, 50, lila_color, show_lila_window)
-button_celeste = Button(width_main//2 + 25, height_main//2 - 25, 100, 50, celeste_color, show_celeste_window)
-button_exit = Button(width_main//2 - 50, height_main//2 + 75, 100, 50, (255, 0, 0), pygame.quit)
+main_window = Window(
+    "*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-POKEMON BATTLE-*-*-*-*-*-*-*-*",
+    (255, 255, 255))
+button_lila = Button(width_main // 2 - 125, height_main // 2 - 25, 100, 50, lila_color, show_lila_window)
+button_celeste = Button(width_main // 2 + 25, height_main // 2 - 25, 100, 50, celeste_color, show_celeste_window)
+button_exit = Button(width_main // 2 - 50, height_main // 2 + 75, 100, 50, (255, 0, 0), pygame.quit)
 
 running = True
 main_window.show()
